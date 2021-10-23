d346d7553f5333ba28c2231c4540b0ae16d87d99
Sumit Kuldharan <kuldharansumitd@gmail.com>
2021-10-23 16:17:36 +0530

    initial commit

---
+++ Task 1/COVID19Tracker/app/src/main/java/com/sumitsoftwares/covid_19tracker/MainActivity.java
@@ -0,0 +1,328 @@
+package com.sumitsoftwares.covid_19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    public static List<Model> modelList = new ArrayList<>();
+    Model model;
+    Adapter adapter;
+    ProgressBar prgbar;
+    TextView status_text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        status_text=findViewById(R.id.status_text);
+        status_text.setVisibility(View.INVISIBLE);
+        prgbar=findViewById(R.id.prgbar);
+        prgbar.setVisibility(View.VISIBLE);
+        fetchData();
+    } private void fetchData() {
+        // The Link Through Which We Can Fetch Data
+        String url = "https://data.covid19india.org/state_district_wise.json";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    prgbar.setVisibility(View.GONE);
+                    JSONObject object = new JSONObject(response);
+
+                    // From that object we are fetching data
+                    JSONObject object1 = object.getJSONObject("Maharashtra");
+                    JSONObject object2 = object1.getJSONObject("districtData");
+                    JSONObject object3 = object2.getJSONObject("Ahmednagar");
+                    JSONObject object4 = object3.getJSONObject("delta");
+
+
+                    String active = object3.getString("active");
+                    String confirmed = object3.getString("confirmed");
+                    String deceased = object3.getString("deceased");
+                    String recovered = object3.getString("recovered");
+
+                    String confInc = object4.getString("confirmed");
+                    String confDec = object4.getString("deceased");
+                    String confRec = object4.getString("recovered");
+
+                    model = new Model("Ahmednagar", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Akola");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Akola", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Amravati");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Amravati", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Aurangabad");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Aurangabad", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Beed");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Beed", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Pune");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Pune", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Mumbai");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Mumbai", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Nashik");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Nashik", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Kolhapur");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Kolhapur", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Dhule");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Dhule", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Satara");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Satara", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    // Creating JSON Object
+                    object3 = object2.getJSONObject("Thane");
+
+                    // From that object we are fetching data
+                    active = object3.getString("active");
+                    confirmed = object3.getString("confirmed");
+                    deceased = object3.getString("deceased");
+                    recovered = object3.getString("recovered");
+                    object4 = object3.getJSONObject("delta");
+                    confInc = object4.getString("confirmed");
+                    confDec = object4.getString("deceased");
+                    confRec = object4.getString("recovered");
+
+                    model = new Model("Thane", confirmed, deceased, recovered, active,
+                            confInc, confDec, confRec);
+                    // placing data into the app using AdapterClass
+                    modelList.add(model);
+
+                    adapter = new Adapter(MainActivity.this, modelList);
+                    listView.setAdapter(adapter);
+
+                    // In case of error it will run
+                }   catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // In case of error it will run
+                if (error instanceof NetworkError) {
+                    status_text.setVisibility(View.VISIBLE);
+                    fetchData();
+                } else if (error instanceof ServerError) {
+                    status_text.setVisibility(View.VISIBLE);
+                    fetchData();
+                } else if (error instanceof AuthFailureError) {
+                    status_text.setVisibility(View.VISIBLE);
+                    fetchData();
+                } else if (error instanceof ParseError) {
+                    status_text.setVisibility(View.VISIBLE);
+                    fetchData();
+                } else if (error instanceof NoConnectionError) {
+                    status_text.setVisibility(View.VISIBLE);
+                    fetchData();
+                } else if (error instanceof TimeoutError) {
+                    status_text.setVisibility(View.VISIBLE);
+                    fetchData();
+                }
+                else
+                {
+                    status_text.setVisibility(View.VISIBLE);
+                    status_text.setText("Something Went Wrong...");
+                    fetchData();
+
+                }
+               // error.printStackTrace();
+                //Toast.makeText(MainActivity.this,"erro"+ error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
+    }
+}
